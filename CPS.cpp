313b07cf8b26e520f185d092c84985de12f08813
Sam <serie@alaska.edu>
2017-03-22 16:09:32 -0800

    adding shape class

---
+++ CPS.cpp
@@ -0,0 +1,30 @@
+int shape::getHeight()
+{
+	return _height;
+}
+int shape::getWidth()
+{
+	return _width;
+}
+
+int shape::getPosX()
+{
+	return _x;
+}
+int shape::getPosY()
+{
+	return _y;
+}
+
+int shape::getUnit()
+{
+	return _inch;
+}
+
+
+
+int main()
+{
+
+	return 0;
+}
---
+++ CPS.h
@@ -0,0 +1,22 @@
+
+
+class shape
+{
+
+private:
+
+	int _height,_width;
+	int _x,_y;
+	int _inch = 72;
+
+public:
+
+	int getHeight();
+	int getWidth();
+
+	int getPosX();
+	int getPosY();
+
+	int getUnit();
+
+};
